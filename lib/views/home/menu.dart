7a471d5369458559ffc1122c50396328785f4b13
laoshupangen <whutch1991@sina.com>
2020-06-09 11:49:44 +0800

    图片

--- lib/views/home/menu.dart
+++ lib/views/home/menu.dart
@@ -7,38 +7,108 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
-  // final int GRID_VIEW_ITEM_COUNT = 4;
-  List images = [
-    'images/upload_bg.png',
+  final int GRID_VIEW_ITEM_COUNT = 4;
+  /*
+  'images/upload_bg.png',
     'images/upload_bg.png',
     'images/upload_bg.png'
+  */ 
+  List images = [
+    {
+      "picUrl":'images/upload_bg.png',
+      "title":'通知标题'
+    },{
+      "picUrl":'images/upload_bg.png',
+      "title":'通知标题2'
+    }
   ];
   @override
   Widget build(BuildContext context) {
     return ListView(
+      children: <Widget>[column1()],
+    );
+  }
+
+  /*卡片1-卡片容器*/
+  Widget column1() {
+    return Card(
+      margin: EdgeInsets.all(15.0),
+      clipBehavior: Clip.antiAlias,
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+      child: swiper(),
+    );
+  }
+
+/*
+Text('更多',style: TextStyle(color:Colors.white))
+*/
+  /*卡片1-轮播图*/
+  Widget swiper() {
+    return Stack(alignment: FractionalOffset(1, 0.9), children: <Widget>[
+      Container(
+        height: 200.0,
+        child: Swiper(
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            // print(images[index]);
+            return swiperItem(imageUrl: images[index]['picUrl'], title: images[index]['title']);
+          },
+          autoplay: true,
+          pagination: new SwiperPagination(),
+        ),
+      ),
+      FlatButton(
+        
+        onPressed: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius:BorderRadius.all(Radius.circular(10.0)),
+            color: Colors.blueAccent
+          ),
+          
+          padding: EdgeInsets.symmetric(horizontal: 5.0,vertical: 3.0),
+          // decoration: ,
+          child:Text('更多',style: TextStyle(color:Colors.white))
+        )
+      )
+    ]);
+  }
+
+  /*轮播-项*/
+  Widget swiperItem({String imageUrl, String title}) {
+    return Stack(
+      alignment: FractionalOffset(0.1, 0.8),
       children: <Widget>[
-        Image.asset('images/upload_bg.png')
+        Image.asset(
+          imageUrl,
+          height: 200.0,
+          fit: BoxFit.cover,
+        ),
+        Text(
+          title,
+          style: TextStyle(color: Colors.white),
+        )
       ],
     );
   }
-  /*卡片1-卡片容器*/
-  Widget column1(){
-    return Card(
-      child: swiper(),
-    );
-  } 
-  /*卡片1-轮播图*/ 
-  Widget swiper(){
-    return Container(
-      height: 150.0,
-      child: Swiper(
-        itemCount: images.length,
-        itemBuilder: (_,index){
-          return Image.asset(images[index]);
-        },
-        autoplay: true,
-        pagination: new SwiperPagination(),
-      ),
-    );
+
+  /*功能按钮*/ 
+  Widget menuButtons(){
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: GRID_VIEW_ITEM_COUNT
+      ), 
+      itemBuilder: (_,index){
+        return Column(
+          children: <Widget>[
+            Image.asset('images/icon/menu_icon_1.png'),
+            Text('平安上报')
+          ],
+        );
+      }
+     );
   }
 }
